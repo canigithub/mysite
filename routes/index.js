99c10377426a810817b314adb806f55c822126bc
Nengyun Zhang <nengyun.zhang@gmail.com>
2016-09-04 13:15:05 -0700

    complete

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Nengyun Zhang' });
 });
 
+
+router.post('/submitmsg', function(req, res) {
+  var msg = {
+    name: req.body.vname,
+    email: req.body.vemail,
+    subject: req.body.vsubject,
+    message: req.body.vmessage,
+  };
+
+  console.log(msg);
+  res.redirect('/');
+});
+
 module.exports = router;
